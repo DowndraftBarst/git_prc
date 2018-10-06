d8249779609ee521df5efcfb3a342dfd40edb5d8
DowndraftBarst <github>
2018-10-06 16:18:44 +0900

    add,コメントを追記
    コメントはやっぱり必要なため

--- Q02.js
+++ Q02.js
@@ -4,14 +4,17 @@ var iniv = 1;
 
 var incrmnt = 2;
 
+//枚数
 var cnt = 100;
 
+//反転処理
 function reverse(cardList,inivalu,incrmntvalu){
     while(inivalu < cnt){
         cardList[inivalu] = !cardList[inivalu];
         inivalu = inivalu + incrmntvalu;
     }
 }
+//カード初期化
 for(var i = 0; i < cnt ; i++ ){
     card[i] = false; 
 }
@@ -21,6 +24,7 @@ while(iniv <= cnt){
     iniv++;
     incrmnt++;
 }
+//出力
 for(var v in card){
     if(card[v] == false){
         console.log(Number(v)+1);
